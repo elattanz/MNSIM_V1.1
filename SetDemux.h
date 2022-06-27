c77ab85b5fbbfc2a4ec00ec5dc7637775338564c
Emily Lattanzio <107049997+elattanz@users.noreply.github.com>
2022-06-27 11:58:14 -0400

    Create SetDemux.h

---
+++ SetDemux.h
@@ -0,0 +1,23 @@
+#indef SETDEMUX_H_
+#define SETDEMUX_H_
+
+//#include "CalculateGate.h"
+#include "Technology.h"
+#include <stdlib.h>
+#include "constant.h"
+
+class SetDemux {
+  public:
+          int Demux_InputNum; //size of gate, ex a 4 means a 1:4 DEMUX
+  
+          SetDemux(Technology tech, int Demux_num);
+          double ~SetDemux();
+          double Demux_Area();
+          double Demux_Latency();
+          double Demux_Power_Leakage();
+          double Demux_Power)Dynamic();
+};
+
+#endif
+
+                                
