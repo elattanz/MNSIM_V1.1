37c8f7dcc5436b5061d969d25ad114ba1da259e1
Emily Lattanzio <107049997+elattanz@users.noreply.github.com>
2022-06-27 14:51:21 -0400

    Update SetShifter.cpp

--- SetShifter.cpp
+++ SetShifter.cpp
@@ -6,8 +6,8 @@ SetShifter::SetShifter(int shifter_num) {
 
 SetShifter::~SetShifter(){}
 
-double Shifter_Area() {
-  switch(Shifter_InputNum)
+double SetShifter::Shifter_Area() {
+  switch(Shifter_InputNum) {
     case 8 :
       return 161.4392;
     case 16 : 
@@ -15,12 +15,12 @@ double Shifter_Area() {
     case 32 : 
       return 589.4408;
   default 
-      return 0;
+      return 0; }
 }
   
  
-double Shifter_Latency() {
-  switch(Shifter_InputNum)
+double SetShifter::Shifter_Latency() {
+  switch(Shifter_InputNum) {
     case 8 :
       return .11;
     case 16 : 
@@ -28,12 +28,12 @@ double Shifter_Latency() {
     case 32 : 
       return .11;
   default 
-      return 0;
+      return 0; }
 }
 
 
-double Shifter_Power_Leakage() {
-  switch(Shifter_InputNum)
+double SetShifter::Shifter_Power_Leakage() {
+  switch(Shifter_InputNum) {
     case 8 :
       return 780.9201e-9;
     case 16 : 
@@ -41,20 +41,20 @@ double Shifter_Power_Leakage() {
     case 32 : 
       return 2.9029e-6;
   default 
-      return 0;
+      return 0; }
 }
 
 
-double Shifter_Power_Dynamic() {
-  switch(Shifter_InputNum)
-    case 8 :
+double SetShifter::Shifter_Power_Dynamic() {
+  switch(Shifter_InputNum) {
+    case 8 : 
       return 14.5705e-6;
     case 16 : 
       return 23.6001e-6;
     case 32 : 
       return 41.6210e-6;
   default 
-      return 0;
+      return 0; }
 }
   
   
