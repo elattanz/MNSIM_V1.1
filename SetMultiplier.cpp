388f9c7f3f32aff7ef02a2825f67d61685873997
Emily Lattanzio <107049997+elattanz@users.noreply.github.com>
2022-06-27 14:53:04 -0400

    Update SetMultiplier.cpp

--- SetMultiplier.cpp
+++ SetMultiplier.cpp
@@ -9,7 +9,7 @@ SetMultiplier::SetMultiplier(int mult_num) {
 SetMultiplier::~SetMultiplier() {}
 
 double SetMultiplier::Mult_Area () {
-  switch (Mult_InputNum)
+  switch (Mult_InputNum) {
    case 8 :
       return 384.826;
    case 16 : 
@@ -17,12 +17,12 @@ double SetMultiplier::Mult_Area () {
    case 32 :
       return 5328.013;
    default
-     return 0;
+     return 0; }
 }
 
 
 double SetMultiplier::Mult_Latency () {
-  switch (Mult_InputNum)
+  switch (Mult_InputNum) {
    case 8 :
       return .91;
    case 16 : 
@@ -30,12 +30,12 @@ double SetMultiplier::Mult_Latency () {
    case 32 :
       return 2.61;
    default
-     return 0;
+     return 0; }
 }
 
 
 double SetMultiplier::Mult_Power_Leakage () {
-  switch (Mult_InputNum)
+  switch (Mult_InputNum) {
    case 8 :
       return 2.2366e-6;
    case 16 : 
@@ -43,11 +43,11 @@ double SetMultiplier::Mult_Power_Leakage () {
    case 32 :
       return 31.0545e-6;
    default
-     return 0;
+     return 0; }
 }
 
 double SetMultiplier::Mult_Power_Dynamic () {
-  switch (Mult_InputNum)
+  switch (Mult_InputNum) {
    case 8 :
       return 96.8856e-6;
    case 16 : 
@@ -55,7 +55,7 @@ double SetMultiplier::Mult_Power_Dynamic () {
    case 32 :
       return 3.057e-3;
    default
-     return 0;
+     return 0; }
 }
 
 
