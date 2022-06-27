ec578550963b8553dd941334665316c38e95c593
Emily Lattanzio <107049997+elattanz@users.noreply.github.com>
2022-06-27 12:22:52 -0400

    Update SetDemux.cpp

--- SetDemux.cpp
+++ SetDemux.cpp
@@ -39,3 +39,35 @@ double SetDemux::Demux_Latency(){
       return 0;
   }
   
+double SetDemux::Demux_Power_Leakage(){
+  switch(Demux_InputNum) {
+    case 4 :
+      return 27.6717e-9;
+    case 8 :
+      return 84.8136e-9;
+    case 16 : 
+      return 232.8362e-9;
+    case 32 :
+      return 422.1485e-9;
+    case 64 :
+      return 740.6179e-9;
+    default 
+      return 0;
+  }
+  
+double SetDemux::Demux_Power_Dynamic(){
+  switch(Demux_InputNum) {
+    case 4 :
+      return 3.1805e-6;
+    case 8 :
+      return 6.202e-6;
+    case 16 : 
+      return 10.4622e-6;
+    case 32 :
+      return 16.6957e-6;
+    case 64 :
+      return 27.7357e-6;
+    default 
+      return 0;
+  }  
+  
