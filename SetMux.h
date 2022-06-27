bdb01e8c21931809e53c737743eb1042ee4ddb28
Emily Lattanzio <107049997+elattanz@users.noreply.github.com>
2022-06-27 14:01:48 -0400

    Update SetMux.h

--- SetMux.h
+++ SetMux.h
@@ -10,7 +10,7 @@ class SetDemux {
   public:
           int Mux_InputNum; //size of gate, ex a 4 means a 4:1 MUX
   
-          SetMux(Technology tech, int mux_num);
+          SetMux(int mux_num);
           double ~SetMux();
           double Mux_Area();
           double Mux_Latency();
