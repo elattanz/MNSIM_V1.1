4e332dd108f791f9ac7b1c7df468300f33895970
Emily Lattanzio <107049997+elattanz@users.noreply.github.com>
2022-07-05 13:11:41 -0400

    Update SetDemux.h

--- SetDemux.h
+++ SetDemux.h
@@ -11,7 +11,7 @@ class SetDemux {
           int Demux_InputNum; //size of gate, ex a 4 means a 1:4 DEMUX
   
           SetDemux(int demux_num);
-          double ~SetDemux();
+          //double ~SetDemux();
           double Demux_Area();
           double Demux_Latency();
           double Demux_Power_Leakage();
