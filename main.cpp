02cc231c37f7b756e5252ae9ac7782f7ba8f3e6a
Emily Lattanzio <107049997+elattanz@users.noreply.github.com>
2022-07-05 14:08:47 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -310,7 +310,7 @@ if (inputParameter->sim_mode == 0)
 							// periphery latency = #MUX*latencyMUX*netrow*netcolumn + #DEMUX*DEMUXlatency*netrow*netcolumn + subcomponents...
 							periph_lat = 4*Mux4.Mux_Latency()*netrow*netcolumn + 1*Demux16.Demux_Latency()*netrow*netcolumn + 4*Demux4.Demux_Area()*netrow*netcolumn;
 							// periphery power = #MUX*MUXleakagePower*netrow*netcolumn + #MUX*MUXdynamicPower*netrow*netcolumn + #DEMUX*DEMUXleakagePower*netrow*netcolumn + #DEMUX*DEMUXdynamicPower*netrow*netcolumn+ subcomponents...
-							periph_power = 4*Mux4.Mux_Power_Leakage()*netrow*netcolumn + 1*Demux16.Demux_Power_Leakage()*netrow*netcolumn + 4*Demux.Demux_Power_Dynamic()*netrow*netcolumn + 4*Mux4.Mux_Power_Dynamic()*netrow*netcolumn + 1*Demux16.Demux_Power_Dynamic()*netrow*netcolumn + 4*Demux4.Demux_Power_Dynamic()*netrow*netcolumn;
+							periph_power = 4*Mux4.Mux_Power_Leakage()*netrow*netcolumn + 1*Demux16.Demux_Power_Leakage()*netrow*netcolumn + 4*Demux4.Demux_Power_Dynamic()*netrow*netcolumn + 4*Mux4.Mux_Power_Dynamic()*netrow*netcolumn + 1*Demux16.Demux_Power_Dynamic()*netrow*netcolumn + 4*Demux4.Demux_Power_Dynamic()*netrow*netcolumn;
 							for (int j=0; j < inputParameter->subCompNum; j++)
 							{
 								periph_area = periph_area + inputParameter->subArea[j]*netrow*netcolumn;
