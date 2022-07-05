81830f5de8babd5d562fb918bc19b4aea6a876eb
Emily Lattanzio <107049997+elattanz@users.noreply.github.com>
2022-07-05 13:19:44 -0400

    Update SetMux.h

--- SetMux.h
+++ SetMux.h
@@ -1,5 +1,6 @@
-//#indef SETMUX_H_
-//#define SETMUX_H_
+#indef SETMUX_H_
+#define SETMUX_H_
+
 
 //#include "CalculateGate.h"
 #include "Technology.h"
@@ -18,4 +19,4 @@ class SetMux {
           double Mux_Power_Dynamic();
 };
 
-//#endif
+#endif
